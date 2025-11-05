fcdd3f401d84353407b3549ea59fa36b6806e794
JGW-Korea <jgw6372@gmail.com>
2025-11-05 18:04:41 +0900

    feat: 라우트 경로 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Route, Routes } from "react-router";
+import { Clock, Stopwatch, Timer, World } from "./pages";
+
 export default function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route index path="/" element={<Clock />}  />
+      <Route index path="/world" element={<World />}  />
+      <Route index path="/stopwatch" element={<Stopwatch />}  />
+      <Route index path="/timer" element={<Timer />}  />
+    </Routes>
+  );
 }
