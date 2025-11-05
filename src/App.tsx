ae01cb0ad8da1bbb1f7610b82d2405535a4b9ad7
JGW-Korea <jgw6372@gmail.com>
2025-11-05 18:58:39 +0900

    update: 모든 라우트 컴포넌트 공통 레이아웃 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router";
 import { Clock, Stopwatch, Timer, World } from "./pages";
+import { Layout } from "./app/index";
+import "./App.scss"
 
 export default function App() {
   return (
     <Routes>
-      <Route index path="/" element={<Clock />}  />
-      <Route index path="/world" element={<World />}  />
-      <Route index path="/stopwatch" element={<Stopwatch />}  />
-      <Route index path="/timer" element={<Timer />}  />
+      <Route element={<Layout />}>
+        <Route index element={<Clock />}  />
+        <Route path="/world" element={<World />}  />
+        <Route path="/stopwatch" element={<Stopwatch />}  />
+        <Route path="/timer" element={<Timer />}  />  
+      </Route>
     </Routes>
   );
 }
