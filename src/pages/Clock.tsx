3bff08b3704c37d9dde744d41a14e0e8d057c230
JGW-Korea <jgw6372@gmail.com>
2025-11-08 02:35:48 +0900

    update: Clock 라우트 컴포넌트 ClockHeader 컴포넌트 추출

--- src/pages/Clock.tsx
+++ src/pages/Clock.tsx
@@ -1,44 +1,16 @@
-import { useRef } from "react";
-import Header from "../shared/header";
-
-import "./clock.style.scss"
+import { useState } from "react";
+import { ClockHeader } from "../widgets/headers";
 
 export default function Clock() {
-  const switchButton = useRef<HTMLDivElement>(null);
-  const switchBtnRight = useRef<HTMLButtonElement>(null);
-  const switchBtnLeft = useRef<HTMLButtonElement>(null);
-  const activeSwitch = useRef<HTMLSpanElement>(null);
+  const [mode, setMode] = useState<boolean>(false);
   
-  const switchLeft = () => {
-    if(!switchBtnLeft.current || !switchBtnRight.current || !activeSwitch.current) return;
-
-    switchBtnRight.current.classList.remove("active-case");
-    switchBtnLeft.current.classList.add("active-case");
-    activeSwitch.current.style.left = "0%";
-  }
-
-  const switchRight = () => {
-    if(!switchBtnLeft.current || !switchBtnRight.current || !activeSwitch.current) return;
-
-    switchBtnRight.current.classList.add("active-case");
-    switchBtnLeft.current.classList.remove("active-case");
-    activeSwitch.current.style.left = "50%";
+  const handleChangeMode = () => {
+    setMode((prev) => !prev);
   }
 
   return (
     <>
-      <Header>
-        <h1>
-          현재 시간
-        </h1>
-        <div className="switch-container">
-          <div ref={switchButton} className="switch-button">
-            <span ref={activeSwitch} className="active" />
-            <button ref={switchBtnLeft} className="switch-button-case left active-case" onClick={switchLeft}>12h</button>
-            <button ref={switchBtnRight} className="switch-button-case right" onClick={switchRight}>24h</button>
-          </div>
-        </div>
-      </Header>
+      <ClockHeader mode={mode} onClick={handleChangeMode} />
     </>
   );
 }
