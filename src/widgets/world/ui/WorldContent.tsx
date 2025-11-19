16e7a81ff82f2d8d0296a1283a4914129605b2a4
JGW-Korea <jgw6372@gmail.com>
2025-11-19 23:18:39 +0900

    feat: 세계 시계 페이지 시계 리스트 레이아웃 구성

--- src/widgets/world/ui/WorldContent.tsx
+++ src/widgets/world/ui/WorldContent.tsx
@@ -12,7 +12,22 @@ export default function WorldContent({ list }: Props) {
       {
         list.length === 0
           ? <span>세계 시계 없음</span>
-          : null
+          : (
+              <ul>
+                <li>
+                    <article>
+                      <div>
+                        <p>오늘, <time>-7시간</time></p>
+                        <h3>
+                          가르보네
+                        </h3>
+                      </div>
+
+                      <p>오후<time>4:17</time></p>
+                    </article>
+                  </li>
+              </ul>
+            )
       }
     </main>
   );
