454d1cf26c657c87a596d2f926c019f66e5c19ce
JGW-Korea <jgw6372@gmail.com>
2025-11-19 22:50:01 +0900

    feat: 세계 시계 라우트 임시 리스트 파일 구성 및 레이아웃 구조 구성

--- src/pages/World.tsx
+++ src/pages/World.tsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from "react";
+import { WorldHeader } from "../widgets/headers";
+import { WorldContent } from "../widgets/world";
+
 export default function World() {
-  return <div>World</div>;
+  const [worldTimeList, setWorldTimeList] = useState<object[]>([]);
+  
+  useEffect(() => {
+    const list = localStorage.getItem("worldTime");
+    if(list) {
+      setWorldTimeList(JSON.parse(list));
+    }
+  }, []);
+
+  return (
+    <>
+      <WorldHeader />
+      
+      {/* 사용자가 추가한 세계 시간 리스트를 출력한다. */}
+      <WorldContent
+        list={worldTimeList}
+      />
+    </>
+  );
 }
