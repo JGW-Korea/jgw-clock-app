3a0f210ad5342c22eb3994023f300344c80fcd35
JGW-Korea <jgw6372@gmail.com>
2025-11-24 00:27:48 +0900

    remove: Stopwatch 전용 헤더 컴포넌트 제거

--- src/widgets/headers/index.ts
+++ src/widgets/headers/index.ts
@@ -1,4 +1,3 @@
 export { default as WorldHeader } from "./ui/WorldHeader"
 export { default as AlarmHeader } from "./ui/AlarmHeader"
-export { default as StopwatchHeader } from "./ui/StopwatchHeader"
 export { default as TimerHeader } from "./ui/TimerHeader"
