f02db4eacc5f6602191c872fd760f7906698c690
JGW-Korea <jgw6372@gmail.com>
2025-11-24 01:47:37 +0900

    update: 기존 탭 네비게이션 아이콘 변경 및 스타일 수정

--- src/widgets/navigator/model/tabs.ts
+++ src/widgets/navigator/model/tabs.ts
@@ -1,11 +1,11 @@
-import Time from "../../../shared/assets/icons/time.svg?react";
 import World from "../../../shared/assets/icons/world.svg?react";
+import Alarm from "../../../shared/assets/icons/alarm.svg?react";
 import Stopwatch from "../../../shared/assets/icons/stopwatch.svg?react";
 import Timer from "../../../shared/assets/icons/timer.svg?react";
 
 export const TABS: { id: number, path: string; label: string; icon: React.FC<React.SVGProps<SVGSVGElement>> }[] = [
-  { id: 1, path: "/", label: "World Clock", icon: Time },
-  { id: 2, path: "/world", label: "Alarms", icon: World },
+  { id: 1, path: "/", label: "World Clock", icon: World },
+  { id: 2, path: "/world", label: "Alarms", icon: Alarm },
   { id: 3, path: "/stopwatch", label: "Stopwatch", icon: Stopwatch },
   { id: 4, path: "/timer", label: "Timers", icon: Timer },
 ];
