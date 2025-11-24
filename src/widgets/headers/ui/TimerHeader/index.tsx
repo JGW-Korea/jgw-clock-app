b1abc2fe63da8ea3ed04d35edaccb856a11c5b55
JGW-Korea <jgw6372@gmail.com>
2025-11-24 12:24:33 +0900

    fix: Vercel Deployment TimerHeader not useed 에러 해결

--- src/widgets/headers/ui/TimerHeader/index.tsx
+++ src/widgets/headers/ui/TimerHeader/index.tsx
@@ -1,20 +1,13 @@
 import style from "./style.module.scss";
 import Header from "../../../../shared/header";
 
-interface Props {
-  timerList: object[]
-}
-
-export default function TimerHeader({ timerList }: Props) {
+export default function TimerHeader() {
   return (
     <Header title="Timers">
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <button className={`${style["glass-button"]} ${style["glass-button__text"]}`}>
           Edit
         </button>
-        {/* <button className={`${style["glass-button"]} ${style["glass-button__icon"]}`}>
-          <PlusIcon />
-        </button> */}
       </div>
     </Header>
   );
