d06858570929e203d0646fe30a25389acf426aa0
JGW-Korea <jgw6372@gmail.com>
2025-11-24 12:30:26 +0900

    fix: Timer 잘못된 Props 속성 전달로 인한 오류 해결

--- src/pages/Timer.tsx
+++ src/pages/Timer.tsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { TimerHeader } from "../widgets/headers";
 
 export default function Timer() {
-  const [timerList, setTimerList] = useState<object[]>([]);
+  // const [timerList, setTimerList] = useState<object[]>([]);
+  // console.log()
     
-  useEffect(() => {
-    const list = localStorage.getItem("worldTime");
-    if(list) {
-      setTimerList(JSON.parse(list));
-    }
-  }, []);
+  // useEffect(() => {
+  //   const list = localStorage.getItem("worldTime");
+  //   if(list) {
+  //     setTimerList(JSON.parse(list));
+  //   }
+  // }, []);
 
   return (
-    <TimerHeader timerList={timerList} />
+    <TimerHeader />
   );
 }
