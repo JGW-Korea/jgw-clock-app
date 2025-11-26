0f4f88e7a6ac186f16d4834e88ea611e17233459
JGW-Korea <jgw6372@gmail.com>
2025-11-26 20:17:35 +0900

    update: 세계 시계 전용 바텀 시트 타입 + 코드 구조 변경

--- src/widgets/bottom-sheet/ui/WorldBottomSheet/components/WorldTimeListItem.tsx
+++ src/widgets/bottom-sheet/ui/WorldBottomSheet/components/WorldTimeListItem.tsx
@@ -3,13 +3,16 @@ import style from "../styles/worldTimeListItem.module.scss";
 interface Props {
   countryName: string;
   zoneName: string;
+  onAppendTimeList: (name: string, to: string) => void;
 }
 
-export default function WorldTimeListItem({ countryName, zoneName }: Props) {
+export default function WorldTimeListItem({ countryName, zoneName, onAppendTimeList }: Props) {
+  const name = zoneName.split("/").at(-1)?.replaceAll("_", " ") + ", " + countryName;
+  
   return (
     <li className={style["list-item"]}>
-      <button>
-        {zoneName.split("/").at(-1)?.replaceAll("_", " ")}, {countryName}
+      <button onClick={() => onAppendTimeList(name, zoneName)}>
+        {name}
       </button>
     </li>
   );
