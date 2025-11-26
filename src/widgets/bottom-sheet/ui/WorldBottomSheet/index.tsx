e3e99ea43cab0d33941714985d3d895a441ce5af
JGW-Korea <jgw6372@gmail.com>
2025-11-26 20:18:20 +0900

    update: 세계 시계 전용 바텀 시트 불필요한 주석문 제거 + 컴포넌트 리스트 속성 변경

--- src/widgets/bottom-sheet/ui/WorldBottomSheet/index.tsx
+++ src/widgets/bottom-sheet/ui/WorldBottomSheet/index.tsx
@@ -4,8 +4,8 @@ import BottomSheet, { type Props } from "../../../../shared/bottom-sheet";
 import type { TimeZoneListDataType, TimeZoneListType } from "../types/timeZone";
 import WorldTimeListItem from "./components/WorldTimeListItem";
 
-export default function WorldBottomSheet({ show, onClick }: Omit<Props, "children">) {
-  const [worldTimeList, setWorldTimeList] = useState<TimeZoneListDataType[]>([]);
+export default function WorldBottomSheet({ show, onClick, onAppendTimeList }: Omit<Props, "children"> & { onAppendTimeList: (name: string, to: string) => void; }) {
+  const [worldTimeListData, setWorldTimeListData] = useState<TimeZoneListDataType[]>([]);
 
   useEffect(() => {
     const fetchTimeZoneDbList = async () => {
@@ -14,14 +14,13 @@ export default function WorldBottomSheet({ show, onClick }: Omit<Props, "childre
           status,
           message,
           zones
-        } } = await axios.get(`http://api.timezonedb.com/v2.1/list-time-zone?key=WIZL9AF2PJUK&format=json`) as TimeZoneListType;
+        } } = await axios.get(`http://api.timezonedb.com/v2.1/list-time-zone?key=${import.meta.env.VITE_TIME_ZONE_API}&format=json`) as TimeZoneListType;
         
         if(status === "FAILED") {
           throw new Error(message);
         }
 
-        console.log(zones)
-        setWorldTimeList(zones);
+        setWorldTimeListData(zones);
       } catch(error) {
         console.error(error);
       }
@@ -33,12 +32,13 @@ export default function WorldBottomSheet({ show, onClick }: Omit<Props, "childre
   return (
     <BottomSheet show={show} onClick={onClick}>
       <ul>
-        {worldTimeList.map(({ countryName, zoneName }) => {
+        {worldTimeListData.map(({ countryName, zoneName }) => {
           return (
             <WorldTimeListItem
               key={zoneName}
               countryName={countryName}
               zoneName={zoneName}
+              onAppendTimeList={onAppendTimeList}
             />
           );
         })}
