190f846bebd564a4e978d05a82729bde58eb04f1
JGW-Korea <jgw6372@gmail.com>
2025-12-02 16:12:50 +0900

    type: WorldHeader 이벤트 핸들러 타입 이름 수정 및 추가

--- src/widgets/headers/ui/WorldHeader/index.tsx
+++ src/widgets/headers/ui/WorldHeader/index.tsx
@@ -1,10 +1,11 @@
 import style from "./style.module.scss";
 import Header from "../../../../shared/header";
-import PlusIcon from "../../../../shared/assets/icons/plus.svg?react"
+import PlusIcon from "../../../../shared/assets/icons/plus.svg?react";
 
 interface Props {
-  worldTimeList: object[]
-  onClick: () => void;
+  worldTimeList: object[];
+  onToggleEditMode: () => void;
+  onOpenAddCitySheet: () => void;
 }
 
 /**
@@ -12,15 +13,21 @@ interface Props {
  * 세계 시계 페이지에서 사용되는 전용 헤더 컴포넌트
  * -------------------------------------
  * - 사용자가 등록한 세계 시계 유무에 따라 `편집` 버튼의 보여짐이 결정된다.
-*/
-export default function WorldHeader({ worldTimeList, onClick }: Props) {
+ */
+export default function WorldHeader({ worldTimeList, onToggleEditMode, onOpenAddCitySheet }: Props) {
   return (
     <Header title="World Clock">
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <button className={`${style["glass-button"]} ${style["glass-button__text"]} ${worldTimeList.length === 0 && style["glass-button__hidden"]}`}>
+        <button
+          className={`
+              ${style["glass-button"]}
+              ${style["glass-button__text"]} ${worldTimeList.length === 0 && style["glass-button__hidden"]}
+            `}
+          onClick={onToggleEditMode}
+        >
           Edit
         </button>
-        <button className={`${style["glass-button"]} ${style["glass-button__icon"]}`} onClick={onClick}>
+        <button className={`${style["glass-button"]} ${style["glass-button__icon"]}`} onClick={onOpenAddCitySheet}>
           <PlusIcon />
         </button>
       </div>
