0279fef02c145edb150c33ee78798cdcd36d90fe
JGW-Korea <jgw6372@gmail.com>
2025-12-04 17:10:06 +0900

    update: PickerWheel 강조 영역 추가

--- src/shared/picker/ui/PickerWheel/index.tsx
+++ src/shared/picker/ui/PickerWheel/index.tsx
@@ -1,8 +1,10 @@
 import { forwardRef } from "react";
+import styles from "./index.module.scss";
 
 interface Props {
   list: ReadonlyArray<string | number>;
   wheel: ReadonlyArray<string | number>;
+  track: ReadonlyArray<string | number>;
   label?: string;
   className?: string;
 }
@@ -15,23 +17,30 @@ interface Props {
  * @param {ReadonlyArray<string | number>} list - 스크롤을 감지하기 위한 상수 배열이다. (무한 스크롤을 하기 위해서는 Wheel의 배열보다 1을 더 크게 설정하여, 마지막 인덱스 도달시 0번으로 되돌리기 위해 사용된다.)
  * @param {ReadonlyArray<string | number>} wheel - 실제 화면에 노출되는 값을 표현하기 위한 상수 배열이다.
 */
-export default forwardRef<HTMLUListElement, Props>(function PickerWheel({ list, wheel, label, className }, ref) {
+export default forwardRef<HTMLUListElement, Props>(function PickerWheel({ list, wheel, track, label, className }, ref) {
   return (
-    <div className={`picker-control picker-control--${className} ${className}`}>
+    <div className={`${styles["picker-control"]} ${styles[`picker-control--${className}`]} ${className && styles[className]}`}>
       {/* 화면에 리스트 내용은 노출되지 않으나, 실제 사용자의 스크롤을 감지하는 영역  */}
-      <ul ref={ref} className={`picker-controller picker-controller--${className}`}>
+      <ul ref={ref} className={`${styles["picker-controller"]} ${styles[`picker-controller--${className}`]}`}>
         {list.map((value, idx) => (
           <li key={idx}>{value}</li>
         ))}
       </ul>
 
       {/* 상단의 ul의 스크롤에 맞춰 실제 화면에 내용을 노출하는 영역 */}
-      <div className={`picker-wheel picker-wheel--${className}`}>
+      <div className={`${styles["picker-wheel"]} ${styles[`picker-wheel--${className}`]}`}>
         {wheel.map((value, idx) => (
           <div key={idx} style={{ "--index": idx } as React.CSSProperties}>
             {value}
           </div>
         ))}
+      </div>
+
+      {/* 스크롤 가운데 바(::after)에 강조(하이라이트)되서 노출되는 데이터 영역 */}
+      <div className={`${styles["picker-track"]}`}>
+        <div className={`${styles["picker-track-holder"]} ${styles[`picker-track--${className}`]}`}>
+          {track.join(" ")}
+        </div>
       </div>
 
       {/* 각 열(Column)의 역할(시간, 분, 초 등)을 설명하는 고정적인 라벨 영역 */}
