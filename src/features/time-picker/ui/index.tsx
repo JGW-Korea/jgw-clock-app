3597b0d151a0be05794097b145dba8e80d36c5cb
JGW-Korea <jgw6372@gmail.com>
2025-12-04 19:34:56 +0900

    update: TimePicker 커스텀 훅 불러온 후 ref 객체 적용

--- src/features/time-picker/ui/index.tsx
+++ src/features/time-picker/ui/index.tsx
@@ -1,12 +1,18 @@
 import { Picker, PickerWheel } from "../../../shared/picker";
 import { HOURS_LIST, HOURS_WHEEL, HOURS_TRACK, MERIDIEM_ITEMS, MINUTES_LIST, MINUTES_TRACK, MINUTES_WHEEL } from "../consts";
+import useTimePickerAnimation from "../model/useTimePickerAnimation";
 
+/**
+ * Picker를 재사용하여 Meridiem / Hours / Minutes 선택 가능한 TimePicker 컴포넌트
+*/
 export default function TimePicker() {
+  const { meridiemRef, hoursRef, minutesRef } = useTimePickerAnimation();
+
   return (
     <Picker>
-      <PickerWheel list={MERIDIEM_ITEMS} wheel={MERIDIEM_ITEMS} track={MERIDIEM_ITEMS} className="meridiem" />
-      <PickerWheel list={HOURS_LIST} wheel={HOURS_WHEEL} track={HOURS_TRACK} className="hours" />
-      <PickerWheel list={MINUTES_LIST} wheel={MINUTES_WHEEL} track={MINUTES_TRACK} className="minutes" />
+      <PickerWheel ref={meridiemRef} list={MERIDIEM_ITEMS} wheel={MERIDIEM_ITEMS} track={MERIDIEM_ITEMS} className="meridiem" />
+      <PickerWheel ref={hoursRef} list={HOURS_LIST} wheel={HOURS_WHEEL} track={HOURS_TRACK} className="hours" />
+      <PickerWheel ref={minutesRef} list={MINUTES_LIST} wheel={MINUTES_WHEEL} track={MINUTES_TRACK} className="minutes" />
     </Picker>
   )
 }
