6f73a51350a57a8f83968d086d6d9f51d8f4f2fa
JGW-Korea <jgw6372@gmail.com>
2025-12-04 12:32:31 +0900

    feat: 전역 Picker 컴포넌트 구조 구성

--- src/shared/picker/ui/Picker/index.tsx
+++ src/shared/picker/ui/Picker/index.tsx
@@ -1,3 +1,16 @@
-export default function Picker() {
-  return <div>index</div>;
+interface PickerProps {
+  children: React.ReactElement[];
+}
+
+/**
+ * Picker 컨테이너 역할을 수행하는 컴포넌트
+*/
+export default function Picker({ children }: PickerProps) {
+  return (
+    <div className="picker">
+      <div className="picker-controls">
+        {children}
+      </div>
+    </div>
+  );
 }
--- src/shared/picker/ui/PickerWheel/index.tsx
+++ src/shared/picker/ui/PickerWheel/index.tsx
@@ -1,3 +1,39 @@
-export default function PickerWheel() {
-  return <div>index</div>;
+import { forwardRef } from "react";
+
+interface Props {
+  items: (string | number)[];
+  label?: string;
+  className?: string;
 }
+
+/**
+ * Picker 컨테이너 각 열(Column)을 담당하는 컴포넌트
+*/
+export default forwardRef<HTMLUListElement, Props>(function PickerWheel({ items, label, className }, ref) {
+  return (
+    <div className={`picker-control picker-control--${className} ${className}`}>
+      {/* 화면에 리스트 내용은 노출되지 않으나, 실제 사용자의 스크롤을 감지하는 영역  */}
+      <ul ref={ref} className={`picker-controller picker-controller--${className}`}>
+        {items.map((value, idx) => (
+          <li key={idx}>{value}</li>
+        ))}
+      </ul>
+
+      {/* 상단의 ul의 스크롤에 맞춰 실제 화면에 내용을 노출하는 영역 */}
+      <div className={`picker-wheel picker-wheel--${className}`}>
+        {items.map((value, idx) => (
+          <div key={idx} style={{ "--index": idx } as React.CSSProperties}>
+            {value}
+          </div>
+        ))}
+      </div>
+
+      {/* 각 열(Column)의 역할(시간, 분, 초 등)을 설명하는 고정적인 라벨 영역 */}
+      {!!label && (
+        <div className={`picker-fixed-label`}>
+          {label}
+        </div>
+      )}
+    </div>
+  );
+});
