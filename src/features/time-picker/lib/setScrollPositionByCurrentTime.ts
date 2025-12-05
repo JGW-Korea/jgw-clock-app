a06222d93b9672af27e260e609a161df43b33947
JGW-Korea <jgw6372@gmail.com>
2025-12-06 05:10:59 +0900

    update: 현재 시간 상태 갱신 명령줄 추가

--- src/features/time-picker/lib/setScrollPositionByCurrentTime.ts
+++ src/features/time-picker/lib/setScrollPositionByCurrentTime.ts
@@ -1,4 +1,4 @@
-import type { TimePickerState } from "../types";
+import type { TimePickerController, TimePickerState } from "../types";
 import { setMeridiem } from "../utils";
 
 /**
@@ -7,7 +7,7 @@ import { setMeridiem } from "../utils";
  * @param {TimePickerState} state - TimePicker 내부에서 사용되는 상태
  * @param {HTMLUListElement[]} refs - 현재 시간
 */
-export default function setScrollPositionByCurrentTime(state: TimePickerState, [meridiem, hours, minutes]: HTMLUListElement[]) {
+export default function setScrollPositionByCurrentTime(state: TimePickerState, [meridiem, hours, minutes]: TimePickerController[]) {
   const now = new Date();
   const [
     currentHours,
@@ -16,9 +16,12 @@ export default function setScrollPositionByCurrentTime(state: TimePickerState, [
   ] = [now.getHours(), now.getMinutes(), now.getHours() >= 12];
 
   // 각 Controller의 실제 DOM에 접근하여 scrollTo 메서드를 통해 스크롤 위치를 변경한다.
-  meridiem.scrollTo(0, currentMeridiem ? meridiem.scrollHeight : 0);
-  hours.scrollTo(0, (hours.scrollHeight / 61) * currentHours);
-  minutes.scrollTo(0, (minutes.scrollHeight / 61) * Math.min(59, currentMinutes));
+  meridiem.element.scrollTo(0, currentMeridiem ? meridiem.element.scrollHeight : 0);
+  hours.element.scrollTo(0, (hours.element.scrollHeight / 61) * currentHours);
+  minutes.element.scrollTo(0, (minutes.element.scrollHeight / 61) * Math.min(59, currentMinutes));
 
+  // 현재 시간에 맞춰서 상태를 업데이트 해준다.
+  state.currentHours = currentHours;
+  state.currentMinutes = Math.min(59, currentMinutes);
   setMeridiem(state, currentMeridiem); // 초기화 한 시간을 기준으로 AM / PM의 상태를 업데이트한다.
 }
--- src/features/time-picker/model/useTimePickerAnimation.ts
+++ src/features/time-picker/model/useTimePickerAnimation.ts
@@ -1,33 +1,56 @@
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react"
 import { createScrollWatcher, setScrollPositionByCurrentTime } from "../lib";
-import type { TimePickerState } from "../types";
+import type { TimePickerController, TimePickerState } from "../types";
+import { getScrollIndex, maintainInfiniteLoop } from "../utils";
 
 export default function useTimePickerAnimation() {
   const meridiemRef = useRef<HTMLUListElement>(null);
   const hoursRef = useRef<HTMLUListElement>(null);
   const minutesRef = useRef<HTMLUListElement>(null);
 
-  const state: TimePickerState = { isPMState: false };
+  const state: TimePickerState = {
+    isPMState: false,
+    currentHours: 0,
+    currentMinutes: 0,
+  };
 
   // useGSAP Hook -> useEffect와 동일하게 의존성(dependencies) 항목에 따른 렌더링･리렌더링 간에 부수효과(side-effect) 로직을 수행한다.
   // 단순히 useEffect와 동일한 것이 아닌 GSAP을 통해 등록한 애니메이션을 "자동으로 해제"하여 메모리 낭비를 방지한다.
   useGSAP(() => {
     if(!meridiemRef.current || !hoursRef.current || !minutesRef.current) return;
 
-    const refs: HTMLUListElement[] = [
-      meridiemRef.current,
-      hoursRef.current,
-      minutesRef.current,
+    const controllers: TimePickerController[] = [
+      { type: "meridiem", element: meridiemRef.current },
+      { type: "hours", element: hoursRef.current },
+      { type: "minutes", element: minutesRef.current },
     ];
 
-    setScrollPositionByCurrentTime(state, refs); // TimePicker의 스크롤 위치를 현재 시간을 기준으로 지정한다.
+    setScrollPositionByCurrentTime(state, controllers); // TimePicker의 스크롤 위치를 현재 시간을 기준으로 지정한다.
 
     // TimePicker Controller에 스크롤 추적을 위한 스크롤 추적 보조 함수를 등록한다.
-    refs.forEach((ref) => {
-      createScrollWatcher(ref, {
+    controllers.forEach((controller) => {
+      createScrollWatcher(controller.element, {
         onStart: () => {},
-        onFrame: () => {},
+        onFrame(number) {
+          maintainInfiniteLoop(controller.element);
+          const index = getScrollIndex(controller.element);
+
+          switch(controller.type) {
+            case "meridiem": {
+              state.isPMState = index === 1;
+              break;
+            }
+            case "hours": {
+              state.currentHours = index;
+              break;
+            }
+            case "minutes": {
+              state.currentMinutes = index;
+              break;
+            }
+          }
+        },
         onStop: () => {}
       });
     });
