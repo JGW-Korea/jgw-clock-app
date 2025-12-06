3cddf4150aa7a48987f867794b7cd0b3764ac680
JGW-Korea <jgw6372@gmail.com>
2025-12-06 18:39:30 +0900

    update: TimePicker ScrollWatcher onStart, onFrame, onStop 로직 수정

--- src/features/time-picker/model/useTimePickerAnimation.ts
+++ src/features/time-picker/model/useTimePickerAnimation.ts
@@ -1,6 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react"
-import { createScrollWatcher, setScrollPositionByCurrentTime } from "../lib";
+import { animationTimeLineFallback, createScrollWatcher, fallbackUpdateController, setScrollPositionByCurrentTime, syncMeridiem } from "../lib";
 import type { TimePickerController, TimePickerState } from "../types";
 import { getScrollIndex, maintainInfiniteLoop } from "../utils";
 
@@ -13,6 +13,13 @@ export default function useTimePickerAnimation() {
     isPMState: false,
     currentHours: 0,
     currentMinutes: 0,
+    prevWrapped: null,
+    prevUnwrapped: null,
+    meridiemOverride: false,
+    lastOverride: false,
+    passiveTrigger: false,
+    meridiemStart: null,
+    meridiemGuard: false
   };
 
   // useGSAP Hook -> useEffect와 동일하게 의존성(dependencies) 항목에 따른 렌더링･리렌더링 간에 부수효과(side-effect) 로직을 수행한다.
@@ -20,28 +27,50 @@ export default function useTimePickerAnimation() {
   useGSAP(() => {
     if(!meridiemRef.current || !hoursRef.current || !minutesRef.current) return;
 
+    // 실제 DOM에 연결한 참조 객체를 배열로 관리한다.
+    // 단, 브라우저에서 animation-timeline을 지원하지 않는 경우 wheel과 track에도 실제 DOM을 연결해준다.
+    // 왜냐하면, animation-timeline을 지원하지 않으면 스크롤이 수행되지 않기 때문에 gsap을 통해 대체를 해줘야하기 때문이다.
     const controllers: TimePickerController[] = [
-      { type: "meridiem", element: meridiemRef.current },
-      { type: "hours", element: hoursRef.current },
-      { type: "minutes", element: minutesRef.current },
+      { type: "meridiem", element: meridiemRef.current, wheel: null, track: null },
+      { type: "hours", element: hoursRef.current, wheel: null, track: null },
+      { type: "minutes", element: minutesRef.current, wheel: null, track: null },
     ];
+    if(!CSS.supports("animation-timeline: scroll()")) { 
+      fallbackUpdateController(controllers);
+    }
 
     setScrollPositionByCurrentTime(state, controllers); // TimePicker의 스크롤 위치를 현재 시간을 기준으로 지정한다.
 
     // TimePicker Controller에 스크롤 추적을 위한 스크롤 추적 보조 함수를 등록한다.
     controllers.forEach((controller) => {
       createScrollWatcher(controller.element, {
-        onStart: () => {},
-        onFrame(number) {
-          maintainInfiniteLoop(controller.element);
-          const index = getScrollIndex(controller.element);
-
+        onStart: () => {
           switch(controller.type) {
             case "meridiem": {
-              state.isPMState = index === 1;
+              state.meridiemStart = getScrollIndex(controller.element, true);
               break;
             }
             case "hours": {
+              state.meridiemGuard = true;
+              break
+            }
+          }
+        },
+        onFrame(number) {
+          // animation-timeline을 지원하지 않는 브라우저인 경우 gsap을 통해 wheel과 track을 스크롤 가능하게 해준다.
+          if(!CSS.supports("animation-timeline: scroll()")) {
+            animationTimeLineFallback(controller);
+          }
+          
+          if(controller.type !== "meridiem") {
+            maintainInfiniteLoop(controller.element);           // 스크롤 수행 도중에 스크롤 위치의 끝단(최하단 / 최상단)에 도달했을 때 스크롤을 멈추지 않고 무한 스크롤을 해주는 보조 함수
+          }
+
+          const index = getScrollIndex(controller.element);   // 
+
+          switch(controller.type) {
+            case "hours": {
+              syncMeridiem(index, state, controllers);
               state.currentHours = index;
               break;
             }
@@ -51,7 +80,26 @@ export default function useTimePickerAnimation() {
             }
           }
         },
-        onStop: () => {}
+        onStop: () => {
+          switch(controller.type) {
+            case "meridiem": {
+              const idx = getScrollIndex(controller.element, true);
+
+              if(state.passiveTrigger) state.passiveTrigger = false;
+              else if(state.meridiemStart !== null && idx !== state.meridiemStart && !state.meridiemGuard) {
+                state.isPMState = idx === 1;
+                state.meridiemOverride = !state.meridiemOverride;
+              }
+
+              state.meridiemStart = null;
+              break;
+            }
+            case "hours": {
+              state.meridiemGuard = false;
+              break;
+            }
+          }
+        }
       });
     });
 
