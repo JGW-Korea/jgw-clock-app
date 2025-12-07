702eec0c5d238b65a5a9d32845be0014f85187ae
JGW-Korea <jgw6372@gmail.com>
2025-12-08 02:31:51 +0900

    refactor: Meridiem Controller의 전환 여부를 결정하는 보조 함수 로직 수정

--- src/features/time-picker/lib/toggleMeridiem.ts
+++ src/features/time-picker/lib/toggleMeridiem.ts
@@ -1,25 +1,37 @@
-import type { TimePickerController, TimePickerState } from "../types";
+import type { TimePickerState } from "../types";
+import { mod60 } from "../utils";
 
-const mod60 = (num: number) => ((num % 60) + 60) % 60;
-
-function toggleMeridiem(state: TimePickerState, element: HTMLUListElement) {
+/**
+ * Meridiem Controller의 AM <-> PM을 자동으로 전환 시키는 보조 함수
+ * 
+ * @param {TimePickerState} state - 현재 TimePicker 내부에서 관리되는 상태
+ * @param {HTMLUListElement} meridiem - 실제 AM <-> PM 자동 전환 시킬 Meridiem Controller
+*/
+function toggleMeridiem(state: TimePickerState, meridiem: HTMLUListElement) {
+  // TimePicker 내부 상태를 변경해준다.
   state.isPMState = !state.isPMState;
   state.passiveTrigger = true;
 
-  if(element) {
-    element.scrollTo({
-      top: state.isPMState ? element.scrollHeight : 0,
-      behavior: "smooth"
-    });
-  }
+  // Meridiem Controller의 스크롤 위치를 변경시킨다.
+  meridiem.scrollTo({
+    top: state.isPMState ? meridiem.scrollHeight : 0,
+    behavior: "smooth"
+  });
 }
 
-export function syncMeridiem(index: number, state: TimePickerState, controllers: TimePickerController[]) {
-  const i = mod60(index + (state.meridiemOverride ? 12 : 0));
+/**
+ * Hours Controller의 스크롤 높낮이에 따라 Meridiem Controller의 변경을 수행해야 될지를 판단하는 보조 함수
+ * 
+ * @param {number} currentIndex - Hours Controller가 스크롤 된 현재 높이
+ * @param {TimePickerState} state - 현재 TimePicker 내부에서 관리되는 상태
+ * @param {HTMLUListElement} meridiem - 실제 AM <-> PM 자동 전환 시킬 Meridiem Controller
+*/
+export function syncMeridiem(currentIndex: number, state: TimePickerState, meridiem: HTMLUListElement) {
+  const index = mod60(currentIndex + (state.meridiemOverride ? 12 : 0));
 
-  if(!state.prevWrapped || !state.prevUnwrapped) {
-    state.prevWrapped = i;
-    state.prevUnwrapped = i;
+  if(state.prevWrapped === null || state.prevUnwrapped === null) {
+    state.prevWrapped = index;
+    state.prevUnwrapped = index;
     state.lastOverride = state.meridiemOverride;
     return;
   }
@@ -31,7 +43,7 @@ export function syncMeridiem(index: number, state: TimePickerState, controllers:
     state.lastOverride = state.meridiemOverride;
   }
 
-  let delta = i - state.prevWrapped;
+  let delta = index - state.prevWrapped;
 
   if(delta > 30) delta -= 60;
   else if(delta < -30) delta += 60;
@@ -41,9 +53,9 @@ export function syncMeridiem(index: number, state: TimePickerState, controllers:
   const after = Math.floor((state.prevUnwrapped + delta) / 12);
 
   if((after - before) & 1) {
-    toggleMeridiem(state, controllers[0].element);
+    toggleMeridiem(state, meridiem);
   }
 
-  state.prevWrapped = i;
+  state.prevWrapped = index;
   state.prevUnwrapped += delta;
 }
