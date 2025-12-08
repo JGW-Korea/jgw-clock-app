db47983309d11a62a02a03191c53ee8f85c0feb2
JGW-Korea <jgw6372@gmail.com>
2025-12-08 14:04:04 +0900

    update: TimePicker 드래그 도중 Bottom Sheet 드래그 중복 발생 막기 위한 Props 전달 구조 변경

--- src/features/time-picker/ui/index.tsx
+++ src/features/time-picker/ui/index.tsx
@@ -2,14 +2,19 @@ import { Picker, PickerWheel } from "../../../shared/picker";
 import { HOURS_LIST, HOURS_WHEEL, HOURS_TRACK, MERIDIEM_ITEMS, MINUTES_LIST, MINUTES_TRACK, MINUTES_WHEEL } from "../consts";
 import useTimePickerAnimation from "../model/useTimePickerAnimation";
 
+interface Props {
+  onMouseOver: () => void;
+  onMouseLeave: () => void;
+}
+
 /**
  * Picker를 재사용하여 Meridiem / Hours / Minutes 선택 가능한 TimePicker 컴포넌트
 */
-export default function TimePicker() {
+export default function TimePicker({ onMouseOver, onMouseLeave }: Props) {
   const { meridiemRef, hoursRef, minutesRef } = useTimePickerAnimation();
 
   return (
-    <Picker>
+    <Picker onMouseOver={onMouseOver} onMouseLeave={onMouseLeave}>
       <PickerWheel ref={meridiemRef} list={MERIDIEM_ITEMS} wheel={MERIDIEM_ITEMS} track={MERIDIEM_ITEMS} className="meridiem" />
       <PickerWheel ref={hoursRef} list={HOURS_LIST} wheel={HOURS_WHEEL} track={HOURS_TRACK} className="hours" />
       <PickerWheel ref={minutesRef} list={MINUTES_LIST} wheel={MINUTES_WHEEL} track={MINUTES_TRACK} className="minutes" />
--- src/shared/picker/ui/Picker/index.tsx
+++ src/shared/picker/ui/Picker/index.tsx
@@ -2,14 +2,16 @@ import styles from "./index.module.scss";
 
 interface PickerProps {
   children: React.ReactElement[];
+  onMouseOver?: () => void;
+  onMouseLeave?: () => void;
 }
 
 /**
  * Picker 컨테이너 역할을 수행하는 컴포넌트
 */
-export default function Picker({ children }: PickerProps) {
+export default function Picker({ children, onMouseOver, onMouseLeave }: PickerProps) {
   return (
-    <div className={styles["picker"]}>
+    <div className={styles["picker"]} onMouseOver={onMouseOver} onMouseLeave={onMouseLeave}>
       <div className={styles["picker-controls"]}>
         {children}
       </div>
