0a15aee3b6fb75e7ad6dd7fca4af40665f160e78
JGW-Korea <jgw6372@gmail.com>
2025-12-08 13:50:12 +0900

    feat: AlarmBottomSheet 컴포넌트 내부 구조 + 스타일 로직 구성

--- src/widgets/bottom-sheet/ui/AlarmBottomSheet/index.tsx
+++ src/widgets/bottom-sheet/ui/AlarmBottomSheet/index.tsx
@@ -1,16 +1,39 @@
-// import { TimePicker } from "../../../../features/time-picker";
-import BottomSheet from "../../../../shared/bottom-sheet";
+import { TimePicker } from "../../../../features/time-picker";
+import { BottomSheet } from "../../../../shared/bottom-sheet";
+import { ALARM_WEEKDAYS } from "../../consts";
+import Check from "../../../../shared/assets/icons/check.svg?react";
+import styles from "./index.module.scss";
+import { useTimePickerDraggable } from "../../model";
 
 interface Props {
-  show: boolean;
-  onClick: () => void;
+  isOpen: boolean;
+  onClose: () => void;
 }
 
-export default function AlarmBottomSheet({ show, onClick }: Props) {
+const selectedWeekdays = [1, 2, 5]; // 임시
+
+export default function AlarmBottomSheet({ isOpen, onClose }: Props) {
+  const { timePickerDraggable, handleTimePickerMouseLeave, handleTimePickerMouseOver } = useTimePickerDraggable();
+
   return (
-    <BottomSheet show={show} onClick={onClick}>
-      <div>
-        {/* <TimePicker /> */}
+    <BottomSheet disableDrag={timePickerDraggable} isOpen={isOpen} onClose={onClose} sheetTitle="Add Alarm" showRightButton={true} onRightButtonClick={() => console.log("Hello")} >
+      <div className={`${styles["alarm-sheet-content"]}`}>
+        <TimePicker onMouseOver={handleTimePickerMouseOver} onMouseLeave={handleTimePickerMouseLeave} />
+
+        <ul className={`${styles["alarm-sheet-content__list"]}`}>
+          {ALARM_WEEKDAYS.map((weekday) => (
+            <li key={weekday.key}>
+              {/* button 내부 자식 요소에 div 등의 */}
+              <button>
+                {weekday.label}
+
+                {selectedWeekdays.includes(weekday.id) && (
+                  <Check width={24} height={24} />
+                )}
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </BottomSheet>
   );
