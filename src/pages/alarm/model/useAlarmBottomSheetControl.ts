947f3ebf62d43016f35cc1fa035ffe6eb63faa68
JGW-Korea <jgw6372@gmail.com>
2025-12-11 18:11:11 +0900

    comment: Alarm Bottom Sheet 제어 커스텀 훅 JSDoc 주석 내용 수정

--- src/pages/alarm/model/useAlarmBottomSheetControl.ts
+++ src/pages/alarm/model/useAlarmBottomSheetControl.ts
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
-/** Alarm Route에서 활용할 Bottom Sheet 컨트롤 커스텀 훅 */
+/**
+ * Alarm Route에서 활용할 Bottom Sheet 컨트롤 커스텀 훅 
+ *
+ * @returns Alarm Bottom Sheet 제어 객체
+ * @property {controls.open} - Alarm Bottom Sheet 활성화 상태
+ * @property {controls.handleOpenBottomSheet} - Alarm Bottom Sheet를 활성화 시키는 메서드
+ * @property {controls.handleCloseBottomSheet} - Alarm Bottom Sheet를 비활성화 시키는 메서드
+*/
 export default function useAlarmBottomSheetControl() {
   const [open, setOpen] = useState<boolean>(false);
 
