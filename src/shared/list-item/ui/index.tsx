2b46a9f1ac6c76bb8878461ab1ea2dbd47ee4ce2
JGW-Korea <jgw6372@gmail.com>
2025-12-13 06:46:16 +0900

    update: 전역 ListItem 컴포넌트 기본 구조 및 스타일 구성

--- src/shared/list-item/ui/index.tsx
+++ src/shared/list-item/ui/index.tsx
@@ -1,7 +1,19 @@
-export default function ListItem() {
+import styles from "./index.module.scss";
+
+interface Props {
+  padding?: number;
+  children: React.ReactElement | React.ReactElement[];
+}
+
+export default function ListItem({ padding = 2, children }: Props) {
   return (
-    <li>
-      
+    <li
+      className={`${styles["list-item"]}`}
+      style={{
+        padding: `${padding}rem 0`,
+      }}
+    >
+      {children}
     </li>
   );
 }
