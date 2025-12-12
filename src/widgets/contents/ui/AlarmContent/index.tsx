089811a27b9eaf7684c15142337518031b26d465
JGW-Korea <jgw6372@gmail.com>
2025-12-13 06:01:53 +0900

    rename: Entities 하위 list-item 디렉토리명 list-item-container로 변경

--- src/widgets/contents/ui/AlarmContent/index.tsx
+++ src/widgets/contents/ui/AlarmContent/index.tsx
@@ -1,16 +1,26 @@
 import { useContext } from "react";
 import { ClockContext } from "../../../../shared/context";
 import styles from "./index.module.scss"
+import { AlarmListItemContainer } from "../../../../entities/list-item-container";
 
 export default function AlarmContent() {
   const { alarmList } = useContext(ClockContext)!;
-  
+
   return (
     <main className={`${styles["layout"]} ${alarmList.length === 0 ? styles["layout-empty"] : ""}`}>
       {
         alarmList.length === 0
           ? <span>No Alarm List</span>
-          : null
+          : (
+            <ul className={styles["layout-list"]}>
+              {alarmList.map((alarm) => (
+                <AlarmListItemContainer
+                  key={alarm.id}
+                  {...alarm}
+                />
+              ))}
+            </ul>
+          )
       }
     </main>
   );
