1948b6386b345bea8896ef03c124b5848ef3da35
JGW-Korea <jgw6372@gmail.com>
2025-12-15 06:31:17 +0900

    update: Layout 컴포넌트 Audio 제어 커스텀 훅 적용

--- src/app/layout/ui/index.tsx
+++ src/app/layout/ui/index.tsx
@@ -1,15 +1,23 @@
 import { Outlet } from "react-router";
 import styles from "./index.module.scss";
 import { TabNavigator } from "../../../widgets/navigator";
+import { useAudioControl } from "../model";
+import { Audio } from "../../../widgets/audio";
 
 export default function Layout() {
-  return (
-    <div className={`${styles["layout"]}`}>
-      <div className={`${styles["layout-content"]}`}>
-        <Outlet />
-      </div>
+  const { audioRef, enable, audioType, handleAudioHidden } = useAudioControl();
 
-      <TabNavigator />
-    </div>
+  return (
+    <>
+      {enable && <Audio ref={audioRef} type={audioType} onClick={handleAudioHidden} /> }
+      
+      <div className={`${styles["layout"]}`}>
+        <div className={`${styles["layout-content"]}`}>
+          <Outlet />
+        </div>
+
+        <TabNavigator />
+      </div>
+    </>
   );
 }
