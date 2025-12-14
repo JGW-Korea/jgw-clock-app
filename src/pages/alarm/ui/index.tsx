71f28d36999242adf24143b9fbd086a4f4572dbc
JGW-Korea <jgw6372@gmail.com>
2025-12-14 14:45:21 +0900

    remove: Alarm Route 불필요한 명령줄 제거

--- src/pages/alarm/ui/index.tsx
+++ src/pages/alarm/ui/index.tsx
@@ -2,17 +2,12 @@ import { AlarmHeader } from "../../../widgets/headers";
 import AlarmBottomSheet from "../../../widgets/bottom-sheet/ui/AlarmBottomSheet";
 import { useAlarmControls } from "../model";
 import AlarmContent from "../../../widgets/contents/ui/AlarmContent";
-import { useContext } from "react";
-import { ClockContext } from "../../../shared/context";
 
 export default function Alarm() {
   const { controlState, handleOpenBottomSheet, handleCloseBottomSheet, handleEditModeActive } = useAlarmControls();
-  const { alarmList } = useContext(ClockContext)!;
 
   return (
     <>
-
-
       {/* Alarm Route 전용 Header 컴포넌트 */}
       <AlarmHeader editMode={controlState.editMode} onClickEditModeActive={handleEditModeActive} onClickOpenSheet={handleOpenBottomSheet} />
       
