ac794d7eb202035718055544b496627e83ce704f
JGW-Korea <jgw6372@gmail.com>
2025-12-14 13:59:40 +0900

    update: AlarmListItemContainer Edit Mode 활성화 시 Delete 버튼 추가

--- src/entities/list-item-container/ui/AlarmListItemContainer/index.tsx
+++ src/entities/list-item-container/ui/AlarmListItemContainer/index.tsx
@@ -2,23 +2,33 @@ import type { AlarmData } from "../../../../shared/context/types";
 import { formatSelectedWeekdays } from "../../model/AlarmListItem";
 import styles from "./index.module.scss";
 
-export default function AlarmListItemContainer({ hours, minutes, weekdays }: AlarmData) {
+interface Props extends AlarmData {
+  editMode: boolean;
+  onDeleteAlarm: (id: number, cb?: () => void) => void;
+  onEditModeActive: () => void;
+}
+
+export default function AlarmListItemContainer({ hours, minutes, weekdays, editMode, id, onDeleteAlarm, onEditModeActive }: Props) {
   return (
-    <article className={`${styles["alarm-list-item"]}`}>
-      {/* 사용자가 설정한 시간과 활성화 상태를 보여주는 레이아웃 */}
-      <div className={`${styles["alarm-list-item__header"]}`}>
-        <time dateTime={`${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`}>
-          <span>{hours % 12 || 12}:{String(minutes).padStart(2, "0")}</span>
-          {hours >= 12 ? "PM" : "AM"}
-        </time>
+    <article className={`${styles["alarm-list-item"]} ${editMode ? styles["edit-mode"] : ""}`}>
+      <button className={`${styles["alarm-list-item__delete-btn"]}`} onClick={() => onDeleteAlarm(id, onEditModeActive)} />
+      
+      <div className={`${styles["alarm-list-item__content"]}`}>
+        {/* 사용자가 설정한 시간과 활성화 상태를 보여주는 레이아웃 */}
+        <div className={`${styles["alarm-list-item__content-header"]}`}>
+          <time dateTime={`${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`}>
+            <span>{hours % 12 || 12}:{String(minutes).padStart(2, "0")}</span>
+            {hours >= 12 ? "PM" : "AM"}
+          </time>
 
-        {/* 알림 활성화 Toggle Switch 컴포넌트 영역 */}
+          {/* 알림 활성화 Toggle Switch 컴포넌트 영역 */}
+        </div>
+
+        {/* 사용자가 선택한 요일을 표시하는 레이아웃 */}
+        <p className={`${styles["alarm-list-item__content-meta"]}`}>
+          {weekdays.length ? formatSelectedWeekdays(weekdays) : "None"}
+        </p>
       </div>
-
-      {/* 사용자가 선택한 요일을 표시하는 레이아웃 */}
-      <p className={`${styles["alarm-list-item__meta"]}`}>
-        {weekdays.length ? formatSelectedWeekdays(weekdays) : "None"}
-      </p>
     </article>
   );
 }
