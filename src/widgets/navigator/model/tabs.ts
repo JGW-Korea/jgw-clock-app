64825a3341ce8bc31c7b942cdbc97814263892ab
JGW-Korea <jgw6372@gmail.com>
2025-12-15 13:00:35 +0900

    update: Tabs 아이템 World Clock -> World로 수정

--- src/widgets/navigator/model/tabs.ts
+++ src/widgets/navigator/model/tabs.ts
@@ -4,7 +4,7 @@ import Stopwatch from "../../../shared/assets/icons/stopwatch.svg?react";
 import Timer from "../../../shared/assets/icons/timer.svg?react";
 
 export const TABS: { id: number, path: string; label: string; icon: React.FC<React.SVGProps<SVGSVGElement>> }[] = [
-  { id: 1, path: "/", label: "World Clock", icon: World },
+  { id: 1, path: "/", label: "World", icon: World },
   { id: 2, path: "/world", label: "Alarms", icon: Alarm },
   { id: 3, path: "/stopwatch", label: "Stopwatch", icon: Stopwatch },
   { id: 4, path: "/timer", label: "Timers", icon: Timer },
