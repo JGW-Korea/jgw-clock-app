97a643f40f21bd62c07bc874a59d1a1aa86a3224
JGW-Korea <jgw6372@gmail.com>
2025-12-17 19:36:15 +0900

    refactor: Alarm / World 헤더 제어에 공통으로 사용되는 훅 로직 하나의 커스텀 훅 파일로 수정

---
+++ src/shared/model/hooks/useHeaderEditSheetControls.ts
@@ -0,0 +1,69 @@
+import { useReducer } from "react";
+import { patchState } from "../../lib/patchState";
+
+type EditMode = { 
+  click: boolean;
+  swipe: boolean;
+}
+
+// Reducer 내부에서 활용할 Action 객체 타입
+export type HeaderControlReducerAction =
+  | { type: "SHEET_SHOW_CHANGE", payload: { key: "sheetOpen", value: boolean } }
+  | { type: "EDIT_ACTIVE", payload: { key: "editMode", type?: keyof EditMode, value: EditMode } };
+
+// Alarm Route 자체에서 관리해야 하는 상태 정보
+export interface HeaderControlState {
+  sheetOpen: boolean;
+  editMode: EditMode;
+}
+
+// 현재 Control State 정보와 action을 전달받아 상태를 업데이트하는 Reducer 함수
+function reducer(state: HeaderControlState, action: HeaderControlReducerAction): HeaderControlState {
+  const { type, payload } = action;
+  
+  switch(type) {
+    // Alarm Route 내부의 Bottom Sheet 활성화 여부 업데이트
+    case "SHEET_SHOW_CHANGE": {
+      return patchState(state, payload.key, payload.value);
+    }
+    
+    // Alarm Route 내부의 Alarm List의 Edit Mode 활성화 여부 업데이트
+    case "EDIT_ACTIVE": {
+      // Click or Swipe 방식을 통해 Edit 모드가 활성화되어 있는 경우 -> 비활성화를 한다.
+      if(payload.value.click || payload.value.swipe) {
+        return patchState(state, payload.key, { click: false, swipe: false });
+      }
+
+      // Edit 모드를 활성화 시킨 발생 원인(Click, Swipe)에 따라 상태를 변경한다.
+      if(payload.type === "click") return patchState(state, payload.key, { click: true, swipe: false });
+      else {
+        return patchState(state, payload.key, { click: false, swipe: true });
+      }
+    }
+  }
+}
+
+/**
+ * Alarm Route에서 활용할 Bottom Sheet 컨트롤 커스텀 훅 
+ *
+ * @returns Alarm Bottom Sheet 제어 객체
+ * @property {controls.open} - Alarm Bottom Sheet 활성화 상태
+ * @property {controls.handleOpenBottomSheet} - Alarm Bottom Sheet를 활성화 시키는 메서드
+ * @property {controls.handleCloseBottomSheet} - Alarm Bottom Sheet를 비활성화 시키는 메서드
+*/
+export default function useHeaderControls() {
+  const [controlState, dispatch] = useReducer(reducer, { 
+    sheetOpen: false,
+    editMode: {
+      click: false,
+      swipe: false
+    } 
+  });
+
+  return {
+    controlState,
+    handleOpenBottomSheet: () => dispatch({ type: "SHEET_SHOW_CHANGE", payload: { key: "sheetOpen", value: true } }),
+    handleCloseBottomSheet: () => dispatch({ type: "SHEET_SHOW_CHANGE", payload: { key: "sheetOpen", value: false } }),
+    handleEditModeActive: (type?: "click" | "swipe") => dispatch({ type: "EDIT_ACTIVE", payload: { key: "editMode", type, value: controlState.editMode } }),
+  }
+}
---
+++ src/shared/model/index.ts
@@ -0,0 +1,1 @@
+export { default as useHeaderEditSheetControls } from "../model/hooks/useHeaderEditSheetControls";
