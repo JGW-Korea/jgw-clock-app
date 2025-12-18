d9295509ff51f45fd407b47d64a618c4de5197a7
JGW-Korea <jgw6372@gmail.com>
2025-12-19 03:54:04 +0900

    comment: Header Control 주석 내용 수정

--- src/shared/model/hooks/useHeaderEditSheetControls.ts
+++ src/shared/model/hooks/useHeaderEditSheetControls.ts
@@ -6,12 +6,12 @@ export type EditMode = {
   swipe: boolean;
 }
 
-// Reducer 내부에서 활용할 Action 객체 타입
+// Reducer Action 객체 타입
 export type HeaderControlReducerAction =
   | { type: "SET_SHEET_VISIBLE", payload: { key: "sheetOpen", value: boolean } }
   | { type: "SET_EDIT_MODE", payload: { key: "editMode", type?: keyof EditMode, value: EditMode } };
 
-// Alarm Route 자체에서 관리해야 하는 상태 정보
+// Header Control 상태
 export interface HeaderControlState {
   sheetOpen: boolean;
   editMode: EditMode;
@@ -44,12 +44,13 @@ function reducer(state: HeaderControlState, action: HeaderControlReducerAction):
 }
 
 /**
- * Alarm Route에서 활용할 Bottom Sheet 컨트롤 커스텀 훅 
+ * Header의 Edit / Bottom Sheet 활성화 등을 관리하는 커스텀 훅
  *
- * @returns Alarm Bottom Sheet 제어 객체
+ * @returns Header에서 제공하는 기능을 관리할 수 있는 제어 객체 반환
  * @property {controls.controlState} - Header에서 관리할 수 있는 상태 정보(Bottom Sheet / Edit Mode)
- * @property {controls.handleOpenBottomSheet} - Header
- * @property {controls.handleCloseBottomSheet} - Alarm Bottom Sheet를 비활성화 시키는 메서드
+ * @property {controls.handleOpenBottomSheet} - Header의 Bottom Sheet를 활성화 시키는 메서드
+ * @property {controls.handleCloseBottomSheet} - Header의 Bottom Sheet를 비활성화 시키는 메서드
+ * @property {controls.handleEditModeActive} - Header의 Edit 버튼을 누르거나, Swipe를 통해 Edit 모드를 활성화 / 비활성화 시키는 메서드
 */
 export default function useHeaderControls() {
   const [controlState, dispatch] = useReducer(reducer, { 
