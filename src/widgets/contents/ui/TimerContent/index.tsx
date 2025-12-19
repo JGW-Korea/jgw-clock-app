856d1a4fc07e1f95eb5417a686abca5eb17fd2da
JGW-Korea <jgw6372@gmail.com>
2025-12-19 19:32:17 +0900

    update: Timers Button Group 구조 구성

--- src/widgets/contents/ui/TimerContent/index.tsx
+++ src/widgets/contents/ui/TimerContent/index.tsx
@@ -5,6 +5,13 @@ export default function TimerContent() {
   return (
     <main className={`${styles["container"]}`}>
       <CountDown styles={styles} />
+
+      <div className={styles["button-group"]}>
+        <button className={styles["reset"]} style={{ display: !false ? "block" : "none" }} >Reset</button>
+        <button className={styles["start"]} style={{ display: !false ? "block" : "none" }} >Start</button>
+        <button className={styles["lap"]} style={{ display: false ? "block" : "none" }} >Lap</button>
+        <button className={styles["stop"]} style={{ display: false ? "block" : "none" }} >Stop</button>
+      </div>
     </main>
   )
 }
