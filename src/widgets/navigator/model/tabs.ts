9f30aa52c7cbb30c5b4e0b6718b1f5983d3bf476
JGW-Korea <jgw6372@gmail.com>
2025-12-22 13:27:26 +0900

    fix: Tab Navigator 상수 데이터 Alarm 탭 world -> alarm path 오타 수정

--- src/widgets/navigator/model/tabs.ts
+++ src/widgets/navigator/model/tabs.ts
@@ -5,7 +5,7 @@ import Timer from "../../../shared/assets/icons/timer.svg?react";
 
 export const TABS: { id: number, path: string; label: string; icon: React.FC<React.SVGProps<SVGSVGElement>> }[] = [
   { id: 1, path: "/", label: "World", icon: World },
-  { id: 2, path: "/world", label: "Alarms", icon: Alarm },
+  { id: 2, path: "/alarm", label: "Alarms", icon: Alarm },
   { id: 3, path: "/stopwatch", label: "Stopwatch", icon: Stopwatch },
   { id: 4, path: "/timer", label: "Timers", icon: Timer },
 ];
