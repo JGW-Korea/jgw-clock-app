c5895efd2a9b2ae0499c1bf0edaa834f23eae17a
JGW-Korea <jgw6372@gmail.com>
2025-12-22 10:42:16 +0900

    chore: Vite 빌드 도구 경로 별칭 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +13,14 @@ export default defineConfig({
     }),
     svgr()
   ],
+  resolve: {
+    alias: [
+      { find: "@app", replacement: path.resolve(__dirname, "src/app") },
+      { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
+      { find: "@widgets", replacement: path.resolve(__dirname, "src/widgets") },
+      { find: "@features", replacement: path.resolve(__dirname, "src/features") },
+      { find: "@entites", replacement: path.resolve(__dirname, "src/entites") },
+      { find: "@shared", replacement: path.resolve(__dirname, "src/shared") },
+    ]
+  }
 })
