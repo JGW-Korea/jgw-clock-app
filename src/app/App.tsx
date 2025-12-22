f8c03663d23224e3730003391fe8656ef543c46b
JGW-Korea <jgw6372@gmail.com>
2025-12-22 13:42:06 +0900

    refactor: 단일 파일 내보내기 슬라이스 하나의 진입점 파일로만 구성

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import { AppProvider } from "./provider";
-import { AppRouter } from "./routers";
+import AppProvider from "./provider";
+import AppRouter from "./routers";
 import "./styles/global.style.scss";
 
 /**
  * 가상 DOM(Virtual DOM)의 최상위 가상 요소(Virtual Root Element) 컴포넌트
- * 
 */
 export default function App() {
   return (
