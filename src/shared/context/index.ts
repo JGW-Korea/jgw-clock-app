f428d295941593b5940f88fdff15f7e66a8bd945
JGW-Korea <jgw6372@gmail.com>
2025-12-22 18:37:08 +0900

    remove: 기존 Shared/context 슬라이스 제거

--- src/shared/context/index.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import type { ClockContextType } from "./types";
-
-/** 전역에서 상태를 공유하기 위한 Context 객체 */
-export const ClockContext = createContext<ClockContextType | null>(null);
