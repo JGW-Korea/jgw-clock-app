800370e943ab3408e70f5aeec02ef574a0609d09
JGW-Korea <jgw6372@gmail.com>
2025-12-22 12:01:42 +0900

    refactor: App.tsx -> src/app/ 레이어로 이동

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router";
-import { World, Alarm, Stopwatch, Timer } from "./pages";
-import { Layout } from "./app/layout";
-import { ClockProvider } from "./app/provider";
-import "./app/styles/global.style.scss";
+import { World, Alarm, Stopwatch, Timer } from "@pages";
+import { Layout } from "./layout";
+import { ClockProvider } from "./provider";
+import "./styles/global.style.scss";
 
 export default function App() {
   return (
