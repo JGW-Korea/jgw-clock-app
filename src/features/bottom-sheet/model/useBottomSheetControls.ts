a696e78f48879b0bedd4dad989d2aa89cb2fb186
JGW-Korea <jgw6372@gmail.com>
2026-01-11 10:36:39 +0900

    refactor: BottomSheet 활성화 제어 상태 관리 로직 별도 커스텀 훅 파일로 분리

--- src/features/bottom-sheet/model/useBottomSheetControls.ts
+++ src/features/bottom-sheet/model/useBottomSheetControls.ts
@@ -1,3 +1,14 @@
+import { useState } from "react";
+
+/**
+ * Bottom Sheet 활성화 제어를 위한 커스텀 훅
+*/
 export default function useBottomSheetControls() {
-  
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return {
+    isOpen,
+    handleOpenBottomSheet: () => setIsOpen(true),
+    handleCloseBottomSheet: () => setIsOpen(false),
+  }
 }
