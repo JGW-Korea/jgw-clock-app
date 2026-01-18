f5940231d9ab845dba45854f3604906f9150256b
JGW-Korea <jgw6372@gmail.com>
2026-01-18 16:36:53 +0900

    remove: 사용되지 않는 shared/utils 슬라이스 제거

--- src/shared/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export { patchState } from "./patchState";
--- src/shared/utils/patchState.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Reducer 함수의 Action.type에 맞는 상태를 변경하기 위한 유틸 함수
-*/
-export function patchState<T>(state: T, key: keyof T, value: T[keyof T]): T {
-  return {
-    ...state,
-    [key]: value
-  }
-}
