fd95c15864541da3cb89d80023e89e4349db88fc
JGW-Korea <jgw6372@gmail.com>
2026-02-03 14:49:44 +0900

    feat: IndexeddB 기반 Cache Storage DB - 데이터 가져오기, 데이터 추가 및 갱신 로직 구성

--- src/shared/db/cacheStorage.ts
+++ src/shared/db/cacheStorage.ts
@@ -1,3 +1,46 @@
+interface CacheStorageRecord<T> {
+  cacheStorageName: string;
+  data: T,
+  expires: number;
+}
+
+/** Cache Storage 객체 저장소 삽입 요청 수행 */
+async function setCacheStorage<T>(cacheStorageName: string, data: T) {
+  const db = await cacheStorageDB;
+
+  // 삽입 요청도 비동기로 수행되기 때문에 Promise 객체를 반환한다.
+  return new Promise((resolve, reject) => {
+    const cacheObjectStore = db.transaction("cacheStorage", "readwrite").objectStore("cacheStorage");
+
+    const record: CacheStorageRecord<T> = {
+      cacheStorageName,
+      data,
+      expires: Date.now() + (1000 * 60 * 60 * 24 * 365),
+    }
+
+    // Cache Storage 객체 저장소에 매개변수로 전달된 데이터를 삽입 요청을 수행한다.
+    // 만일 동일한 기본 키에 대한 데이터가 존재하면 데이터 삽입이 아닌 갱신이 일어난다.
+    const cacheObjectStoreSetRequest = cacheObjectStore.put(record);
+    cacheObjectStoreSetRequest.onsuccess = () => resolve(cacheObjectStoreSetRequest.result);
+    cacheObjectStoreSetRequest.onerror = () => reject(cacheObjectStoreSetRequest.error);
+  });
+}
+
+/** Cache Storage 객체 저장소 조회 요청 수행 */
+async function getCacheStorage<T>(cacheStorageName: string): Promise<CacheStorageRecord<T> | undefined> {
+  const db = await cacheStorageDB;
+  
+  // 조회 요청도 비동기로 수행되기 때문에 Promise 객체를 반환한다.
+  return new Promise((resolve, reject) => {
+    const cacheObjectStore = db.transaction("cacheStorage", "readonly").objectStore("cacheStorage");
+    
+    // Cache Storage 객체 저장소에 매개변수로 전달된 데이터 조회 요청을 수행한다.
+    const cacheObjectStoreGetRequest = cacheObjectStore.get(cacheStorageName);
+    cacheObjectStoreGetRequest.onsuccess = () => resolve(cacheObjectStoreGetRequest.result as CacheStorageRecord<T> | undefined);
+    cacheObjectStoreGetRequest.onerror = () => reject(cacheObjectStoreGetRequest.error);
+  });
+}
+
 // IndexedDB에 생성한 데이터베이스 정보를 담는 cacheStorageDB 변수를 생성한다.
 // 이때 Promise 객체를 이용해서 생성하는 이유는 IndexedDB는 논블로킹 방식의 비동기 처리를 지원하기 때문이다.
 // 이로 인해, 다른 CRUD 작업을 수행하는 함수에서 cacheStorageDB 변수에 접근할 경우 undefined로 처리되기 때문에 IndexedDB 데이터베이스에 접근하지 못하게 된다.
@@ -34,4 +77,4 @@ const cacheStorageDB: Promise<IDBDatabase> = new Promise((resolve, reject) => {
   }
 });
 
-export { cacheStorageDB };
+export { cacheStorageDB, getCacheStorage, setCacheStorage };
