e4f02e29539b9a166db230ee5f5f2e19e268acca
JGW-Korea <jgw6372@gmail.com>
2026-02-16 20:22:10 +0900

    chore: vite-plugin-purgecss 패키지 + 설정 모두 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig, type PluginOption } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 import path from 'path';
-import purgecss from "vite-plugin-purgecss";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,11 +12,6 @@ export default defineConfig({
       },
     }),
     svgr(),
-    purgecss({
-      content: ["./src/**/*.tsx", "./src/**/*.jsx", "./index.html"],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      safelist: ["html", "body"]
-    }) as PluginOption,
   ],
   server: {
     port: Number(process.env.PORT) || 5173,
