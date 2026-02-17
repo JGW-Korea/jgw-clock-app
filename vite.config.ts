67e9789d1eb009eae381ead5e2398009d7b9e597
JGW-Korea <jgw6372@gmail.com>
2026-02-17 22:00:50 +0900

    reset: Vite CSS 트랜스파일 lightningcss 재적용

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,14 @@ export default defineConfig({
   server: {
     port: Number(process.env.PORT) || 5173,
   },
+  css: {
+    transformer: "lightningcss",
+    lightningcss: {
+      cssModules: {
+        dashedIdents: true
+      }
+    }
+  },
   resolve: {
     alias: [
       { find: "@app", replacement: path.resolve(__dirname, "src/app") },
