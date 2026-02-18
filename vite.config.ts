76ca641015faa183833ef734f083619b15fe1669
JGW-Korea <jgw6372@gmail.com>
2026-02-19 00:42:41 +0900

    test: Chunk 분리 파일 <script>로 수정 플러그인 제거

--- vite.config.ts
+++ vite.config.ts
@@ -13,14 +13,7 @@ export default defineConfig({
       },
     }),
     svgr(),
-    visualizer(),
-    {
-      name: "replace-preload-with-script",
-      transformIndexHtml(html) {
-        const preloadRegex = /<link rel="modulepreload" [^>]*href="([^"]*vendor-[^"]*)"[^>]*>/g;
-        return html.replace(preloadRegex, (_, href) => `<script type="module" crossorigin src="${href}"></script>`);
-      }
-    }
+    visualizer()
   ],
   server: {
     port: Number(process.env.PORT) || 5173,
