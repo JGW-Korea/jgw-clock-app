afea72c38848c8d22143df1aa047b17ef6389194
JGW-Korea <jgw6372@gmail.com>
2026-02-19 02:04:10 +0900

    test: TabNavigator lazy 선언

--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -1,6 +1,9 @@
 import { Outlet } from "react-router";
 import styles from "./index.module.scss";
-import { TabNavigator } from "@widgets/navigator";
+import { lazy } from "react";
+// import { TabNavigator } from "@widgets/navigator";
+
+const TabNavigator = lazy(() => import("@widgets/navigator").then((module) => ({ default: module.TabNavigator })));
 
 /**
  * 선언형 React Router에서 모든 라우트 간 공통적으로 사용되는 Layout 컴포넌트
