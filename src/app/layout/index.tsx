ada890160fb1d6454536d20a43d0212b310db338
JGW-Korea <jgw6372@gmail.com>
2026-02-19 02:09:54 +0900

    reset: Tab Navigator lazy 제거

--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -1,9 +1,6 @@
-import { Outlet } from "react-router";
 import styles from "./index.module.scss";
-import { lazy } from "react";
-// import { TabNavigator } from "@widgets/navigator";
-
-const TabNavigator = lazy(() => import("@widgets/navigator").then((module) => ({ default: module.TabNavigator })));
+import { Outlet } from "react-router";
+import { TabNavigator } from "@widgets/navigator";
 
 /**
  * 선언형 React Router에서 모든 라우트 간 공통적으로 사용되는 Layout 컴포넌트
