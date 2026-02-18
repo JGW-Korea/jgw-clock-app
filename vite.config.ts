fe96099341e40be5f3c24346973c6f3c39f473f0
JGW-Korea <jgw6372@gmail.com>
2026-02-18 21:57:23 +0900

    chore: rollup-plugin-visualizer 개발 의존성 설치 및 Vite Plugin 등록

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 import path from 'path';
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
       },
     }),
     svgr(),
+    visualizer()
   ],
   server: {
     port: Number(process.env.PORT) || 5173,
