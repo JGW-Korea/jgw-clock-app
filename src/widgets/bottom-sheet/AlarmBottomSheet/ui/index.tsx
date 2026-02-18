73806292a66a07ed294ca7321f6348f393d5713e
JGW-Korea <jgw6372@gmail.com>
2026-02-19 00:27:07 +0900

    test: React Sheet Modal lazy 기반으로 변경

--- src/widgets/bottom-sheet/AlarmBottomSheet/ui/index.tsx
+++ src/widgets/bottom-sheet/AlarmBottomSheet/ui/index.tsx
@@ -1,14 +1,17 @@
 import Check from "@shared/assets/icons/check.svg?react";
 import styles from "./index.module.scss";
-import { BottomSheet } from "@shared/ui";
+// import { BottomSheet } from "@shared/ui";
 import { TimePicker } from "@features/time-picker";
 import { useAlarmSchedule, useTimePickerDraggable } from "../model";
 import { ALARM_WEEKDAYS } from "../consts";
+import { lazy } from "react";
 
 interface Props {
   isOpen: boolean;
   onClose: () => void;
 }
+
+const BottomSheet = lazy(() => import("@shared/ui").then((moduel) => ({ default: moduel.BottomSheet })));
 
 export default function AlarmBottomSheet({ isOpen, onClose }: Props) {
   const { alarmState, dispatch, handleTimeChange, handleToggleWeekday, handleAddAlarm } = useAlarmSchedule();
