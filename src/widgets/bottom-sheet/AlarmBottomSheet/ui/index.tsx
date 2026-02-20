4768c21decb6ba2e31202c32ab05a82f26c9cb88
JGW-Korea <jgw6372@gmail.com>
2026-02-20 19:12:41 +0900

    test: TimePicker 컴포넌트 lazy 적용

--- src/widgets/bottom-sheet/AlarmBottomSheet/ui/index.tsx
+++ src/widgets/bottom-sheet/AlarmBottomSheet/ui/index.tsx
@@ -1,10 +1,10 @@
 import CheckSVGComponent from "@shared/assets/icons/check.svg?react";
 import styles from "./index.module.scss";
 import BottomSheet from "@shared/ui/BottomSheet";
-import { TimePicker } from "@features/time-picker";
+// import { TimePicker } from "@features/time-picker";
 import { useAlarmSchedule, useTimePickerDraggable } from "../model";
 import { ALARM_WEEKDAYS } from "../consts";
-// import { lazy } from "react";
+import { lazy } from "react";
 
 interface Props {
   isOpen: boolean;
@@ -12,7 +12,7 @@ interface Props {
 }
 
 // const BottomSheet = lazy(() => import("@shared/ui/BottomSheet"));
-// const TimePicker = lazy(() => import("@features/time-picker").then((moduel) => ({ default: moduel.TimePicker })));
+const TimePicker = lazy(() => import("@features/time-picker").then((moduel) => ({ default: moduel.TimePicker })));
 
 export default function AlarmBottomSheet({ isOpen, onClose }: Props) {
   const { alarmState, dispatch, handleTimeChange, handleToggleWeekday, handleAddAlarm } = useAlarmSchedule();
